7813b9a6d269dcd49800dae7c62cda24f83b76a4
Aaron Claydon <aaron@argil.app>
2020-09-03 21:24:38 +0100

    Basic login and register view

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,8 +6,21 @@ Vue.use(VueRouter)
   const routes: Array<RouteConfig> = [
   {
     path: '/login',
-    name: 'Login',
-    component: () => import('../views/Login.vue')
+    name: 'Auth/Login',
+    component: () => import('../views/Auth/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'Auth/Register',
+    component: () => import('../views/Auth/Register.vue')
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "dashboard" */ '../views/Dashboard.vue')
   },
   {
     path: '/app',
